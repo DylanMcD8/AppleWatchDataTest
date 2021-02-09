b27a0d05b05495a37c3127e784adec800705c6e3
Dylan McDonald <41933237+DylanMcD8@users.noreply.github.com>
2021-02-09 01:42:57 -0500

    Add files via upload

---
+++ SendToWatchTest/SendToWatchTest WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,43 @@
+//
+//  InterfaceController.swift
+//  SendToWatchTest WatchKit Extension
+//
+//  Created by Dylan McDonald on 2/8/21.
+//
+
+import WatchKit
+import Foundation
+import WatchConnectivity
+
+class InterfaceController: WKInterfaceController {
+    
+    @IBOutlet weak var messageLabel: WKInterfaceLabel!
+    
+//    override func awake(withContext context: Any?) {
+//        // Configure interface objects here.
+//    }
+//
+//    override func willActivate() {
+//        // This method is called when watch view controller is about to be visible to user
+//        if WCSession.isSupported() {
+//            let session = WCSession.default
+//            session.delegate = self
+//            session.activate()
+//        }
+//    }
+//
+//    override func didDeactivate() {
+//        // This method is called when watch view controller is no longer visible
+//    }
+//
+//    func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+//        WKInterfaceDevice().play(.click)
+//        messageLabel.setText("Message Received!")
+////        messageLabel.setText(message)
+//    }
+//
+//    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+//
+//    }
+    
+}
